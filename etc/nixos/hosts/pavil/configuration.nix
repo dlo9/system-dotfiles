594221e235ebcca889d577039d0f61c11fd7e549
David Orchard <if_coding@fastmail.com>
2022-04-30 17:00:00 -0700

    Use a folder for each host

--- etc/nixos/hosts/pavil/configuration.nix
+++ etc/nixos/hosts/pavil/configuration.nix
@@ -4,8 +4,8 @@
 
 {
   imports = [
-    ./system.nix
-    ./polkit.nix
+    ../../system.nix
+    ../../polkit.nix
   ];
 
   # Secrets
@@ -13,7 +13,7 @@
 
   # You can avoid this by adding a string to the full path instead, i.e.
   # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
-  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFile = ../../secrets/secrets.yaml;
 
   # This will automatically import SSH keys as age keys
   sops.age.sshKeyPaths = [
