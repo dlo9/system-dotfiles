dc5223ee803e4ef69ebd6645a80560b122dab066
David Orchard <if_coding@fastmail.com>
2022-05-28 11:28:54 -0700

    Adjust wayland variabes

--- etc/nixos/sys/graphical/default.nix
+++ etc/nixos/sys/graphical/default.nix
@@ -30,11 +30,14 @@ in
       enable = true;
       wrapperFeatures.gtk = true;
       extraSessionCommands = ''
+        # SDL:
         export SDL_VIDEODRIVER=wayland
-        export QT_QPA_PLATFORM=wayland
+        # QT (needs qt5.qtwayland in systemPackages):
+        export QT_QPA_PLATFORM=wayland-egl
         export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+        # Fix for some Java AWT applications (e.g. Android Studio),
+        # use this if they aren't displayed properly:
         export _JAVA_AWT_WM_NONREPARENTING=1
-        export MOZ_ENABLE_WAYLAND=1
       '';
       extraPackages = with pkgs; [
         # Core
