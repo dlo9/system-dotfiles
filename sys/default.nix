ccb33e5640d3f86f55bc23b3e323f6c4c3870ce6
David Orchard <if_coding@fastmail.com>
2022-05-30 20:26:53 -0700

    Add tentative binary cache setting

--- sys/default.nix
+++ sys/default.nix
@@ -33,6 +33,20 @@ in
     # Configs that don't warrent their own module yet #
     ###################################################
 
+    # Binary caches
+    # 22.05 only?
+    # nix = {
+    #   settings = {
+    #     substituters = [
+    #       "https://cache.nixos.org/"
+    #       "https://nix-community.cachix.org"
+    #     ];
+    #     trusted-public-keys = [
+    #       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    #     ];
+    #   };
+    # };
+
     # Docker
     virtualisation.docker = {
       enable = true;
