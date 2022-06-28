21bc53eeab9bd2e7a83009a435f07610dc48771b
David Orchard <if_coding@fastmail.com>
2022-06-27 18:33:38 -0700

    Ace doesn't support EFI

--- flake.nix
+++ flake.nix
@@ -147,9 +147,13 @@
 
         ace = buildSystem "ace" "x86_64-linux" [
           ({ config, ... }: {
-            boot.loader.grub.mirroredBoots = [
-              { devices = [ "/dev/disk/by-id/ata-KINGSTON_SNS4151S332G_50026B724500626D" ]; efiSysMountPoint = "/boot/efi0"; path = "/boot/efi0/EFI"; }
-            ];
+            boot.loader = {
+              efi.canTouchEfiVariables = false;
+
+              grub.mirroredBoots = [
+                { devices = [ "/dev/disk/by-id/ata-KINGSTON_SNS4151S332G_50026B724500626D" ]; efiSysMountPoint = "/boot/efi0"; path = "/boot/efi0/EFI"; }
+              ];
+            };
           })
         ];
 
