dae0838817ad86f64de5301c2c0839b04a3a6ba3
David Orchard <if_coding@fastmail.com>
2022-07-29 18:11:19 -0700

    Partial sway config

--- home/gui.nix
+++ home/gui.nix
@@ -625,5 +625,27 @@ in
       # File syncing
       syncthing.enable = true;
     };
+
+    wayland.windowManager.sway = {
+      enable = true;
+      config = {
+        bars = [
+          {
+            command = "\${pkgs.waybar}/bin/waybar";
+            colors = {
+              background = "$base00";
+              separator = "$base01";
+              statusline = "$base04";
+
+              focusedWorkspace = { border = "$base05"; background = "$base0D"; text = "$base00"; };
+              activeWorkspace = { border = "$base05"; background = "$base03"; text = "$base00"; };
+              inactiveWorkspace = { border = "$base03"; background = "$base01"; text = "$base05"; };
+              urgentWorkspace = { border = "$base08"; background = "$base08"; text = "$base00"; };
+              bindingMode = { border = "$base00"; background = "$base0A"; text = "$base00"; };
+            };
+          }
+        ];
+      };
+    };
   };
 }
