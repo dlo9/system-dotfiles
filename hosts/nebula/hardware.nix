0de191681b99f4caa7d6a9e799d190dab5f74af3
David Orchard <if_coding@fastmail.com>
2022-08-25 17:35:03 -0700

    Re-order nebula filesystems

--- hosts/nebula/hardware.nix
+++ hosts/nebula/hardware.nix
@@ -9,40 +9,10 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" "wl" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
-
-  fileSystems."/" =
-    {
-      device = "pool/nixos/root";
-      fsType = "zfs";
-    };
-
-  fileSystems."/var/lib/containerd/io.containerd.snapshotter.v1.zfs" =
-    {
-      device = "pool/containerd";
-      fsType = "zfs";
-    };
-
-  fileSystems."/root" =
-    {
-      device = "pool/home/root";
-      fsType = "zfs";
-    };
-
-  fileSystems."/home/david" =
-    {
-      device = "pool/home/david";
-      fsType = "zfs";
-    };
-
-  fileSystems."/boot/efi0" =
-    {
-      device = "/dev/disk/by-uuid/C8F3-D20C";
-      fsType = "vfat";
-    };
 
   swapDevices =
     [{ device = "/dev/disk/by-uuid/644fd77a-246d-4240-9648-8cb5ceccd5ca"; }];
@@ -52,10 +22,39 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp2s0f0u8u4.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
 
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   # high-resolution display
   hardware.video.hidpi.enable = lib.mkDefault true;
+
+  fileSystems."/" =
+    {
+      device = "pool/nixos/root";
+      fsType = "zfs";
+    };
+
+  fileSystems."/boot/efi0" =
+    {
+      device = "/dev/disk/by-uuid/C8F3-D20C";
+      fsType = "vfat";
+    };
+
+  fileSystems."/home/david" =
+    {
+      device = "pool/home/david";
+      fsType = "zfs";
+    };
+
+  fileSystems."/root" =
+    {
+      device = "pool/home/root";
+      fsType = "zfs";
+    };
+
+  fileSystems."/var/lib/containerd/io.containerd.snapshotter.v1.zfs" =
+    {
+      device = "pool/containerd";
+      fsType = "zfs";
+    };
 }
