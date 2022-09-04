09d2eb89e34064d56515a0b766f343f1578f1337
David Orchard <if_coding@fastmail.com>
2022-09-03 23:51:15 -0700

    Generate hardware

--- hosts/generate-hardware-config.sh
+++ hosts/generate-hardware-config.sh
@@ -7,7 +7,7 @@ config="/etc/nixos/hosts/$hostname/hardware.nix"
 touch "$config"
 
 if [ -n "$root" ]; then
-  extraArgs+="--root $root"
+  extraArgs="--root $root"
 fi
 
 # Must use `sudo` so that all mounts are visible
