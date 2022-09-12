6d1427ddff255c97291f3b975bbbcfa95b228226
David Orchard <if_coding@fastmail.com>
2022-09-11 18:03:37 -0700

    Add samba to drywell

--- hosts/drywell/default.nix
+++ hosts/drywell/default.nix
@@ -93,5 +93,68 @@ in
         ];
       };
     };
+
+    # Samba
+    services.samba-wsdd.enable = true; # make shares visible for windows 10 clients
+
+    networking.firewall.allowedTCPPorts = [
+      5357 # wsdd
+    ];
+
+    networking.firewall.allowedUDPPorts = [
+      3702 # wsdd
+    ];
+
+    services.samba = {
+      enable = true;
+      openFirewall = true;
+
+      shares = {
+        smb = {
+          path = "/slow/smb/";
+          browseable = "yes";
+          #content = "Network storage";
+          "read only" = "no";
+          #"guest ok" = "yes";
+          "create mask" = "0644";
+          "directory mask" = "0755";
+          "force user" = "michael";
+          "force group" = "users";
+          "valid users" = "+samba";
+          #"valid users" = "david michael";
+        };
+      };
+
+      # Users must be added with `sudo smbpasswd -a <user>`
+      extraConfig = let
+        tailscaleNat = "100.64.0.0/10";
+        in ''
+          workgroup = WORKGROUP
+          server string = drywell2
+          netbios name = drywell2
+          security = user
+          #use sendfile = yes
+          #max protocol = smb2
+          hosts allow = 100.64. 192.168. 127.0.0.1 localhost
+          hosts deny = 0.0.0.0/0
+          guest account = nobody
+          map to guest = bad user
+        '';
+    };
+
+    # Users
+    users.users.michael = {
+      uid = 1001;
+      isNormalUser = true;
+      hashedPassword = "$6$S/H.nEE7XEPdyO6v$ENulPNgv2WGmwdCD7zluMNasQ/wPFdc61wjxC2/aFXcl9dLvbMzzeSeVI9V5dxycJaojJRFUtqKYNPJIX767P1";
+      createHome = false;
+      #shell = pkgs.fish;
+      extraGroups = [
+        "samba"
+      ];
+    };
+
+    users.groups.samba = {};
+    users.users.david.extraGroups = [ "samba" ];
   };
 }
