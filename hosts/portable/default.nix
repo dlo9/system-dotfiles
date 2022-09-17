6e1a1f638895a36e85e39349ae9a1dd692317128
David Orchard <if_coding@fastmail.com>
2022-09-16 18:21:31 -0700

    Add correct firmware for MINIX

--- hosts/portable/default.nix
+++ hosts/portable/default.nix
@@ -24,7 +24,7 @@
       ];
     };
 
-    networking.interfaces.enp1s0.useDHCP = true;
+    networking.interfaces.useDHCP = true;
     hardware.cpu.intel.updateMicrocode = true;
     hardware.cpu.amd.updateMicrocode = true;
 
@@ -33,10 +33,35 @@
       "kvm-amd"
     ];
 
+    # MINIX Neo Z83-4 Max uses a Broadcom wifi module which uses upstream firmware, but does not have the right name
+    # (in `dmesg` look for `brcmfmac43455-sdio.MINIX-Z83-4.txt`)
+    hardware.firmware = [
+      (pkgs.callPackage
+        ({ stdenv, linux-firmware }:
+
+          stdenv.mkDerivation {
+            pname = "minix-wireless-firmware";
+            version = linux-firmware.version;
+
+            phases = [ "installPhase" ];
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p "$out/lib/firmware/brcm"
+
+              cp "${linux-firmware}/lib/firmware/brcm/brcmfmac43455-sdio.MINIX-NEO Z83-4.txt" "$out/lib/firmware/brcm/brcmfmac43455-sdio.MINIX-Z83-4.txt"
+
+              runHook postInstall
+            '';
+          })
+        { }
+      )
+    ];
+
     # Ethernet modules for remote boot login
     boot.initrd.availableKernelModules = [
-      "r8169"
-      "iwlwifi"
+      "r8169" # Realtek ethernet
+      "iwlwifi" # Intel wifi
     ];
   };
 }
