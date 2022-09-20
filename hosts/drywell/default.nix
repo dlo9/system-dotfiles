1d8eca58460a954b0e4cdeedd18be31c6452c8c8
David Orchard <if_coding@fastmail.com>
2022-09-19 20:09:09 -0700

    Use USB on drywell

--- hosts/drywell/default.nix
+++ hosts/drywell/default.nix
@@ -43,10 +43,12 @@ in
     boot.loader = {
       grub.mirroredBoots = [
         { devices = [ "/dev/disk/by-id/nvme-Force_MP500_17037932000122530025" ]; efiSysMountPoint = "/boot/efi0"; path = "/boot/efi0/EFI"; }
+        { devices = [ "/dev/disk/by-id/usb-Leef_Supra_0171000000030148-0:0" ]; efiSysMountPoint = "/boot/efi1"; path = "/boot/efi1/EFI"; }
       ];
     };
 
-    networking.interfaces.enp1s0.useDHCP = true;
+    networking.useDHCP = true;
+    networking.interfaces.enp2s0.useDHCP = true;
 
     # Ethernet modules for remote boot login
     boot.initrd.kernelModules = [
