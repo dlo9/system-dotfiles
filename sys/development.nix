091077a22af435f27bfecf4926c25fce02c18f8b
David Orchard <if_coding@fastmail.com>
2023-03-01 18:09:29 -0800

    Add nix-ld so that vscode remote SSH works

--- sys/development.nix
+++ sys/development.nix
@@ -26,5 +26,12 @@ in
       jq
       yq
     ];
+
+    # Allow running unpatched binaries, including vscode-serer
+    programs.nix-ld.enable = true;
+    environment.sessionVariables = {
+      NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.stdenv.cc.cc ];
+      NIX_LD = "${pkgs.glibc}/lib/ld-linux-x86-64.so.2";
+    };
   };
 }
