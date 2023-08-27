8bb1f0c34e01fc67c9d1846eba0173558aab15ca
David Orchard <if_coding@fastmail.com>
2023-08-26 17:12:07 -0700

    Add host: mallow

---
+++ hosts/mallow/exports.nix
@@ -0,0 +1,12 @@
+{
+  ssh-keys = {
+    host = {
+      ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOCqUby1xlAoTg/S7MORJaMnsyQrnzg0TRc+E8yTwJOh root@mallow";
+      rsa = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCmG/v7+pWM6WF6qGPJmY6JSOSeu1WXLddTK7Q5hfAeqfA+6A3tSgDYf8+WIPbc4JRJabZOSvHedBbuzPgGmqMHMlpztlqyU5tXVq9GOjPhArl6Ik6sx9NTZhYu5ecz0uGCIZw8QjLE/dff32MISbjW+dk2D6HoM/NwYJP4ju18TX3fdx1H/UlnCqBlzSXMw3F7sd5XQK6hbwlHivkQAeqoDXsJRt3nN8ciznuVpPRlOjeMeQfVIc32aYcAWaJLgau7xgbKSVPtY9K2kBdWDjuWqKhuj0LRKuDC5I+Buaxb907shnjqRUBI5We8voTGHP14ghqiGrpDMgJ5zfekQGg+xw4gJaAv8qlLfrQf2sYOAMTfL/buc4y1YWYpwYK5i0fDXGzccwoCZV2VHnBrCwGKVp9I87UIVYGxfRL2zVLWlhpI1FKnMF4muwOOkftBRDrDs+hTLHAUfoGXrhNpi/v6NuZyqlbDwbsk5tfUOgisXGoThqdi5gapyLqzSEVaUZE= root@mallow";
+    };
+
+    dorchard = {
+      ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIISY2r8Q9e5yi7fpQPETuifKHE8LdcCcD+3v+P8Nioor dorchard@mallow";
+    };
+  };
+}
