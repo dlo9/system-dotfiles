22e4f762484f976d9b3c87f9c3b781d8f5755c97
David Orchard <dorchard@apexclearing.com>
2023-08-29 08:57:42 -0700

    Refactor shkd

--- hosts/mallow/default.nix
+++ hosts/mallow/default.nix
@@ -130,7 +130,11 @@ with lib; {
     '';
   };
 
-  services.skhd = {
+  services.skhd =
+    let
+      modifier = "alt";
+    in
+  {
     # Don't forget to disable "Secure Keyboard Entry" by opening the terminal application
     enable = true;
     skhdConfig = ''
@@ -139,59 +143,58 @@ with lib; {
       # ioreg -l -w 0 | perl -nle 'print $1 if /"kCGSSessionSecureInputPID"=(\d+)/' | uniq | xargs -I{} ps -p {} -o comm=
 
       # Focus window
-      alt - left : yabai -m window --focus west || yabai -m display --focus west
-      alt - right : yabai -m window --focus east || yabai -m display --focus east
-      alt - up : yabai -m window --focus north || yabai -m display --focus north
-      alt - down : yabai -m window --focus south || yabai -m display --focus south
+      ${modifier} - left : yabai -m window --focus west || yabai -m display --focus west
+      ${modifier} - right : yabai -m window --focus east || yabai -m display --focus east
+      ${modifier} - up : yabai -m window --focus north || yabai -m display --focus north
+      ${modifier} - down : yabai -m window --focus south || yabai -m display --focus south
 
       # Move managed window
-      alt - space : yabai -m window --toggle split
-      alt + shift - left : yabai -m window --swap west || (yabai -m window --display west && yabai -m display --focus west)
-      alt + shift - right : yabai -m window --swap east || (yabai -m window --display east && yabai -m display --focus east)
-      alt + shift - up : yabai -m window --swap north || (yabai -m window --display north && yabai -m display --focus north)
-      alt + shift - down : yabai -m window --swap south || (yabai -m window --display south && yabai -m display --focus south)
+      ${modifier} - space : yabai -m window --toggle split
+      ${modifier} + shift - left : yabai -m window --swap west || (yabai -m window --display west && yabai -m display --focus west)
+      ${modifier} + shift - right : yabai -m window --swap east || (yabai -m window --display east && yabai -m display --focus east)
+      ${modifier} + shift - up : yabai -m window --swap north || (yabai -m window --display north && yabai -m display --focus north)
+      ${modifier} + shift - down : yabai -m window --swap south || (yabai -m window --display south && yabai -m display --focus south)
 
       # Fullscreen
-      alt - f : yabai -m window --toggle native-fullscreen
-      #alt - f : yabai -m window --toggle zoom-fullscreen
+      ${modifier} - f : yabai -m window --toggle native-fullscreen
 
       # Close
-      alt + shift - q : yabai -m window --close
+      ${modifier} + shift - q : yabai -m window --close
 
       # Terminal
-      alt - return : /Applications/Alacritty.app/Contents/MacOS/alacritty
+      ${modifier} - return : ${pkgs.alacritty}/bin/alacritty
 
       # Resizing
-      alt + ctrl - left : yabai -m window --resize left:-100:0 || yabai -m window --resize right:-100:0
-      alt + ctrl - right : yabai -m window --resize right:100:0 || yabai -m window --resize left:100:0
-      alt + ctrl - up : yabai -m window --resize top:0:-100 || yabai -m window --resize bottom:0:-100
-      alt + ctrl - down : yabai -m window --resize bottom:0:100 || yabai -m window --resize top:0:100
+      ${modifier} + ctrl - left : yabai -m window --resize left:-100:0 || yabai -m window --resize right:-100:0
+      ${modifier} + ctrl - right : yabai -m window --resize right:100:0 || yabai -m window --resize left:100:0
+      ${modifier} + ctrl - up : yabai -m window --resize top:0:-100 || yabai -m window --resize bottom:0:-100
+      ${modifier} + ctrl - down : yabai -m window --resize bottom:0:100 || yabai -m window --resize top:0:100
 
       # Toggle focus & center window
-      alt + shift - space : yabai -m window --toggle float && yabai -m window --grid 4:4:1:1:2:2 && yabai -m window --focus
+      ${modifier} + shift - space : yabai -m window --toggle float && yabai -m window --grid 4:4:1:1:2:2 && yabai -m window --focus
 
       # Focus monitor
-      alt - 1 : yabai -m display --focus 1
-      alt - 2 : yabai -m display --focus 2
-      alt - 3 : yabai -m display --focus 3
+      ${modifier} - 1 : yabai -m display --focus 1
+      ${modifier} - 2 : yabai -m display --focus 2
+      ${modifier} - 3 : yabai -m display --focus 3
 
       # Send window to monitor
-      alt + shift - 1 : yabai -m window --display 1 && yabai -m display --focus 1
-      alt + shift - 2 : yabai -m window --display 2 && yabai -m display --focus 2
-      alt + shift - 3 : yabai -m window --display 3 && yabai -m display --focus 3
+      ${modifier} + shift - 1 : yabai -m window --display 1 && yabai -m display --focus 1
+      ${modifier} + shift - 2 : yabai -m window --display 2 && yabai -m display --focus 2
+      ${modifier} + shift - 3 : yabai -m window --display 3 && yabai -m display --focus 3
 
       # Set split direction
-      alt - v : yabai -m window --insert south
-      alt - h : yabai -m window --insert east
+      ${modifier} - v : yabai -m window --insert south
+      ${modifier} - h : yabai -m window --insert east
 
       # Reset split ratio
-      alt + ctrl - r : yabai -m window --ratio abs:0.5
+      ${modifier} + ctrl - r : yabai -m window --ratio abs:0.5
 
       # Restart yabai
-      alt + shift - r : pkill yabai; yabai &
+      ${modifier} + shift - r : pkill yabai; yabai &
 
       # Enable/disable yabai tiling
-      alt + shift - e : if [ "$(yabai -m config layout)" == "bsp" ]; then yabai -m config layout float; else yabai -m config layout bsp; fi
+      ${modifier} + shift - e : if [ "$(yabai -m config layout)" == "bsp" ]; then yabai -m config layout float; else yabai -m config layout bsp; fi
 
       # Toggle dock visibility
       cmd - d: osascript -e 'tell application "System Events" to set the autohide of the dock preferences to not (get the autohide of the dock preferences)'
