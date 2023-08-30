80a12f3966ce1f6bd29cacef6657f698fc4db021
David Orchard <dorchard@apexclearing.com>
2023-08-30 12:29:26 -0700

    Add mac settings

--- hosts/mallow/default.nix
+++ hosts/mallow/default.nix
@@ -262,4 +262,24 @@ in {
   };
 
   home-manager.users.dorchard = import ./home.nix;
+
+  # System settings
+  system.defaults = {
+    NSGlobalDomain = {
+      # Disable natural scrolling
+      "com.apple.swipescrolldirection" = false;
+
+      # Hide menu bar, since spacebar is used instead
+      _HIHideMenuBar = true;
+
+      AppleShowAllFiles = true;
+    };
+
+    dock = {
+      autohide = true;
+      tilesize = 16;
+      largesize = 128;
+      magnification = true;
+    };
+  };
 }
