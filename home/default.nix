dcf1f6a3fd2133b4a69d6604afa0b767f934a3b4
David Orchard <if_coding@fastmail.com>
2023-09-27 11:31:35 -0700

    Add wrap shim generation

--- home/default.nix
+++ home/default.nix
@@ -31,6 +31,28 @@ with lib; {
     sessionVariables = {
       SOPS_AGE_KEY_FILE = "/var/sops-age-keys.txt";
     };
+
+    file = {
+      ".wrap/generate.sh".text = ''
+        #!/bin/sh
+
+        set -e
+
+        dir="$(dirname "$0")"
+
+        if [[ -z "$@" ]] || [[ -z "$dir" ]]; then
+          echo "usage: generate.sh <wrap alias>..."
+          exit 1
+        fi
+
+        for name in "$@"; do
+          bin="$dir/shims/$name"
+
+          printf "%s\n" "#!/bin/sh" "wrap $name \"\$@\"" > "$bin"
+          chmod +x "$bin"
+        done
+      '';
+    };
   };
 
   # Allow user-installed fonts
