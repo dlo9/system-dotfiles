46d9673bf2bee42efa1917b27810a40f3967e9ae
David Orchard <if_coding@fastmail.com>
2023-10-30 20:32:03 -0700

    Fix grub issue

--- hosts/cuttlefish/hardware/quirks.nix
+++ hosts/cuttlefish/hardware/quirks.nix
@@ -7,4 +7,7 @@ with lib; {
   # Mount special filesystems before services
   systemd.services.containerd.requires = ["var-lib-containerd-io.containerd.snapshotter.v1.overlayfs.mount"];
   systemd.services.docker.requires = ["var-lib-docker-overlay2.mount"];
+
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.efi.canTouchEfiVariables = false;
 }
