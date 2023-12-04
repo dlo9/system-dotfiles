1f6e970d5a9fa1ed050c9144c9a9dc3399893105
David Orchard <if_coding@fastmail.com>
2023-12-03 16:49:08 -0800

    Set pstate

--- hosts/cuttlefish/hardware/quirks.nix
+++ hosts/cuttlefish/hardware/quirks.nix
@@ -6,4 +6,9 @@
 with lib; {
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.efi.canTouchEfiVariables = false;
+
+  # ondemand isn't supported since the amd-pstate driver is in use
+  # https://www.reddit.com/r/linux/comments/15p4bfs/amd_pstate_and_amd_pstate_epp_scaling_driver
+  powerManagement.cpuFreqGovernor = "powersave";
+  systemd.services.cpufreq.serviceConfig.ExecStartPost = ''/bin/sh -c 'echo "power" | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference' '';
 }
