361906a1150cb3c47ca626152d613dedd2e8980b
David Orchard <if_coding@fastmail.com>
2024-01-14 23:19:21 -0800

    Add generated hardware

--- hosts/bee/hardware/generated.nix
+++ hosts/bee/hardware/generated.nix
@@ -17,15 +17,56 @@
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/5a22e348-4fa6-483c-8fcd-8d41d64583ec";}
+  ];
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s20f0u5u2c2.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  #fileSystems."/" = {
+  #  device = "fast/nixos/root";
+  #  fsType = "zfs";
+  #};
+
+  #fileSystems."/boot" = {
+  #  device = "/dev/disk/by-uuid/D713-3B93";
+  #  fsType = "vfat";
+  #};
+
+  #fileSystems."/home/david" = {
+  #  device = "fast/home/david";
+  #  fsType = "zfs";
+  #};
+
+  #fileSystems."/nix" = {
+  #  device = "fast/nixos/nix";
+  #  fsType = "zfs";
+  #};
+
+  #fileSystems."/var/lib/containerd/io.containerd.content.v1.content" = {
+  #  device = "fast/containerd/content";
+  #  fsType = "zfs";
+  #};
+
+  #fileSystems."/var/lib/containerd/io.containerd.snapshotter.v1.overlayfs" = {
+  #  device = "fast/containerd/snapshotter";
+  #  fsType = "zfs";
+  #};
+
+  #fileSystems."/var/lib/docker" = {
+  #  device = "fast/docker";
+  #  fsType = "zfs";
+  #};
+
+  #fileSystems."/zfs" = {
+  #  device = "fast/zfs";
+  #  fsType = "zfs";
+  #};
 }
