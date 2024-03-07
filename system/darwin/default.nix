2b7ee85ad1346fd9a2926e6e8689f69f2b829b38
David Orchard <if_coding@fastmail.com>
2024-03-07 12:37:19 -0800

    Workaround sops-nix darwin incompatibility

--- system/darwin/default.nix
+++ system/darwin/default.nix
@@ -2,6 +2,7 @@
   config,
   inputs,
   pkgs,
+  lib,
   ...
 }: {
   imports = [
@@ -12,4 +13,12 @@
     ./yabai.nix
     inputs.home-manager.darwinModules.home-manager
   ];
+
+  # Temporary workaround since sops-nix checks a systemd options,
+  # which doesn't exist in nix-darwin
+  options.systemd = lib.mkOption {
+    type = lib.types.attrs;
+    default = {};
+    readOnly = true;
+  };
 }
