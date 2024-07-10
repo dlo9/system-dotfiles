e3d58b7b4dd4a1316795354586fdc84c50ae4535
David Orchard <dorchard@apexfintechsolutions.com>
2024-07-10 16:32:03 -0700

    Add gh-dash

--- home/cli/developer-tools.nix
+++ home/cli/developer-tools.nix
@@ -22,6 +22,27 @@ with lib; {
         nixd # Nix language server
         nix-prefetch
         pkgs.unstable.terminaltexteffects
+
+        gh-dash
+      ];
+    };
+
+    # https://github.com/dlvhdr/gh-dash
+    xdg.configFile."gh-dash/config.yml".text = builtins.toJSON {
+      prSections = [
+        {
+          title = "My Pull Requests";
+          filters = "is:open author:@me";
+          layout.author.hidden = true;
+        }
+        {
+          title = "Needs My Review";
+          filters = "is:open review-requested:@me -team-review-requested:apex-fintech-solutions/engineering";
+        }
+        {
+          title = "Involved";
+          filters = "is:open involves:@me - author:@me";
+        }
       ];
     };
   };
