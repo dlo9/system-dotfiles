242e3d280c783504fbae73edced6ff8983ea5d0f
David Orchard <if_coding@fastmail.com>
2024-07-15 23:59:27 -0700

    Don't convert from yaml every time

--- pkgs/lib/fromYAML.nix
+++ pkgs/lib/fromYAML.nix
@@ -1,16 +1,21 @@
 {
   runCommand,
   yj,
-}: yamlFile:
-builtins.fromJSON (
-  builtins.readFile (
-    runCommand "from-yaml"
-    {
-      allowSubstitutes = false;
-      preferLocalBuild = true;
-    }
-    ''
-      ${yj}/bin/yj -yj < "${yamlFile}" > "$out"
-    ''
+}: yamlFile: let
+  # Put the file in the store, so that the derivation doesn't change every time.
+  # Otherwise, the file path changes each time anything in the repo changes,
+  # delaying build times
+  inputFile = builtins.toFile "input.yaml" (builtins.readFile yamlFile);
+in
+  builtins.fromJSON (
+    builtins.readFile (
+      runCommand "from-yaml"
+      {
+        allowSubstitutes = false;
+        preferLocalBuild = true;
+      }
+      ''
+        ${yj}/bin/yj -yj < "${inputFile}" > "$out"
+      ''
+    )
   )
-)
