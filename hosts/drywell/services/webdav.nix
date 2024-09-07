758f9d1f2b858f340932ef5263e7366d554c8326
David Orchard <if_coding@fastmail.com>
2024-09-07 12:38:54 -0700

    Refactor drywell webdav

--- hosts/drywell/services/webdav.nix
+++ hosts/drywell/services/webdav.nix
@@ -6,23 +6,7 @@
   ...
 }:
 with builtins;
-with lib; let
-  # nix-shell -p terraform-providers.htpasswd --run "htpasswd -nB david"
-  htpasswd = pkgs.writeText "htpasswd" ''
-    david:$2y$05$pe8DCM.Q8ojQZtYUnM.HP..Lw3IOfpywuVD6QLD5yZ3QNVm0ZyOPi
-    sue:$2y$05$XKP3L/DsHkLMszi0sWRHAO9Yz3xgaRfyiRqH8cqYcATyQDzWRKlBO
-    michael:$2y$05$1zRGWNFtecV1mxcrO8lSz.5sOmUqZSXRtwBw8W.soEh83Ryl37iS2
-  '';
-
-  webdav-root = pkgs.linkFarm "webdav-root" {
-    "michael" = "/slow/documents/michael";
-    "sue" = "/slow/documents/sue";
-    "sue-server" = "/slow/documents/sue";
-
-    "backup/sue" = "/slow/backups/sue";
-    "backup/michael" = "/slow/backups/michael";
-  };
-in {
+with lib; {
   config = {
     services.webdav-server-rs = {
       enable = true;
@@ -41,7 +25,11 @@ in {
         };
 
         htpasswd.default = {
-          inherit htpasswd;
+          htpasswd = pkgs.writeText "htpasswd" ''
+            david:$6$lfXZQaVisXg6Gjqz$1dTcCAbHKnMjk.PJs0EUpSsG773FXma54tqaLGdMbDBmb7v848m/tA.46oI0ProdPd6b7u49U0d6h8Jq7wQK4/
+            sue:$2y$05$XKP3L/DsHkLMszi0sWRHAO9Yz3xgaRfyiRqH8cqYcATyQDzWRKlBO
+            michael:$2y$05$1zRGWNFtecV1mxcrO8lSz.5sOmUqZSXRtwBw8W.soEh83Ryl37iS2
+          '';
         };
 
         unix = {
@@ -52,8 +40,15 @@ in {
         location = [
           {
             route = ["/(*path)"];
-            directory = webdav-root;
             methods = ["webdav-rw"];
+            directory = pkgs.linkFarm "webdav-root" {
+              "michael" = "/slow/documents/michael";
+              "sue" = "/slow/documents/sue";
+              "sue-server" = "/slow/documents/sue";
+
+              "backup/sue" = "/slow/backups/sue";
+              "backup/michael" = "/slow/backups/michael";
+            };
 
             # TODO: make defaults
             handler = "filesystem";
