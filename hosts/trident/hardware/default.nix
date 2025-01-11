5acfff33d932a0c18cf58a980a87b3eb76a465fc
David Orchard <if_coding@fastmail.com>
2025-01-11 12:38:43 -0800

    Cleanup

--- hosts/trident/hardware/default.nix
+++ hosts/trident/hardware/default.nix
@@ -12,7 +12,6 @@ with lib; {
     raspberry-pi-4
 
     #./disks.nix
-    # ./quirks.nix
     ./generated.nix
   ];
 
@@ -41,68 +40,7 @@ with lib; {
     deviceTree = {
       enable = true;
 
-      overlays = let
-        btt-display = pkgs.fetchFromGitHub {
-          owner = "bigtreetech";
-          repo = "TFT43-DIP";
-          rev = "a4409952502d7e09521454c530ff728bd5856542";
-          hash = "sha256-mlkuc5cWJ0qjYV9KgHRTF3JxzOeOe7e++4D54I7p+uY=";
-        };
-
-        tft43-display = pkgs.runCommand "tft43-display" {} ''
-          mkdir $out
-
-          ${pkgs.libraspberrypi}/bin/dtoverlay \
-            -d ${config.hardware.deviceTree.dtbSource}/overlays \
-            -D vc4-kms-dpi-generic \
-            rgb666-padhi=true \
-            clock-frequency=32000000 \
-            hactive=800 \
-            hfp=16 \
-            hsync=1 \
-            hbp=46 \
-            vactive=480 \
-            vfp=7 \
-            vsync=3 \
-            vbp=23 \
-            backlight-gpio=19 \
-            rotate=0
-
-            mv dry_run.dtbo "$out/tft43-display.dtbo"
-        '';
-      in [
-        #{
-        #  name = "rpi-tft43-overlay";
-        #  dtboFile = "${btt-display}/gt911_btt_tft43_dip.dtbo";
-        #  dtsFile = "${btt-display}/gt911_btt_tft43_dip.dts";
-        #}
-        #{
-        #  name = "vc4-kms-dpi-generic";
-        #  dtboFile = "${pkgs.raspberrypifw}/share/raspberrypi/boot/overlays/vc4-kms-dpi-generic.dtbo";
-
-          #dtsFile = pkgs.fetchurl {
-          #  url = "https://raw.githubusercontent.com/raspberrypi/linux/refs/heads/rpi-6.6.y/arch/arm/boot/dts/overlays/vc4-kms-dpi-generic-overlay.dts";
-          #  hash = "sha256-+YJKM5S79Y1K+T1ihGXvGAutZkJci/gJzQTKfO4h7gw=";
-          #};
-        #}
-        #{
-        #  name = "vc4-kms-v3d";
-        #  # Required by the generic display below
-        #  dtboFile = "${config.hardware.deviceTree.dtbSource}/overlays/vc4-kms-v3d-pi4.dtbo";
-        #}
-        #{
-        #  name = "vc4-kms-dsi-7inch";
-        #  dtboFile = "${tft43-display}/vc4-kms-dsi-7inch.dtbo";
-        #}
-        #{
-        #  name = "tft43-display";
-        #  dtboFile = "${tft43-display}/tft43-display.dtbo";
-        #}
-        #{
-        #  name = "generic-dpi-display";
-        #  # Alternate path when using stock kernel: ${pkgs.raspberrypifw}/share/raspberrypi/boot/overlays
-        #  dtboFile = "${config.hardware.deviceTree.dtbSource}/overlays/vc4-kms-dpi-generic.dtbo";
-        #}
+      overlays = [
         {
           # This rotates the display and is a modification of:
           # https://github.com/NixOS/nixos-hardware/blob/8870dcaff63dfc6647fb10648b827e9d40b0a337/raspberry-pi/4/touch-ft5406.nix#L48-L49
